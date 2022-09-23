6d2fea2879d55ea3135c0f6d52f715d8b9a2299c
raflirahman <raflirahman375@gmail.com>
2022-09-23 09:11:13 +0700

    add tugas 2

---
+++ tugas2.js
@@ -0,0 +1,8 @@
+var number =20;
+var string = 'satu';
+var boolean = true;
+var ada = "kosong";
+var undifined;
+var aray = [2,3,4,5,6,7];
+
+console.log(number,string,boolean,null,undifined,aray,aray[2]);
